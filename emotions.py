adf60ee0fdfcd7c65f39904620c85e729ac369bb
Abhishek Soni <abhisheksoni2720@gmail.com>
2018-06-10 14:43:04 +0530

    split into lines

--- emotions.py
+++ emotions.py
@@ -4,6 +4,23 @@ import numpy as np
 import indicoio
 
 indicoio.config.api_key = '26837191c44dd6a7560a688e3307920d'
-chat=open('Iqra.txt')
 
 
+def cleanText(filename, ownerName="Abhishek Soni"):
+
+    chat = open(filename)
+    personName = filename.split('.')[0]
+    chatText = chat.read()
+    pattern = "(\d+\/\d+\/\d+)(,)(\s)(\d+:\d+)(\s)(\w+)(\s)(-)(\s)(\w+)(\s\w+)?(:)"
+
+    regex = re.compile(pattern, flags=re.M)
+
+    clean = regex.sub("", chatText)
+    lines = []
+    for line in clean.splitlines():
+        lines.append(line.strip())
+
+    return lines
+
+
+linesList =cleanText('innaya.txt')
