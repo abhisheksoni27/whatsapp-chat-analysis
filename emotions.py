62a451e196bbb75eeb4e732c7fb0631304751956
Abhishek Soni <abhisheksoni2720@gmail.com>
2018-06-10 15:58:49 +0530

    formatting

--- emotions.py
+++ emotions.py
@@ -17,7 +17,8 @@ def cleanText(filename, ownerName="Abhishek Soni"):
     chat = open(filename)
     personName = filename.split('.')[0]
     chatText = chat.read()
-    pattern = "(\d+\/\d+\/\d+)(,)(\s)(\d+:\d+)(\s)(\w+)(\s)(-)(\s)(\w+)(\s\w+)?(:)"
+    # 01/09/17, 11:34 PM - Subia:
+    pattern = "(\d+\/\d+\/\d+)(,)(\s)(\d+:\d+)(\s)(\w+)(\s)(-)(\s\w+)*(:)"
 
     mediaPattern = "(\<Media omitted\>)"
     regexMedia = re.compile(mediaPattern, flags=re.M)
@@ -34,36 +35,39 @@ def cleanText(filename, ownerName="Abhishek Soni"):
     return lines
 
 
-linesList = cleanText('innaya.txt')
-neutral, negative, positive = 0, 0, 0
+def analyze(name):
+    linesList = cleanText(name + '.txt')
+    neutral, negative, positive = 0, 0, 0
 
-for index, sentence in enumerate(linesList):
-    print("Processing {0}%".format(str((index * 100) / len(linesList))))
+    for index, sentence in enumerate(linesList):
+        print("Processing {0}%".format(str((index * 100) / len(linesList))))
 
-    scores = sentient_analyzer.polarity_scores(sentence)
-    scores.pop('compound', None)
+        scores = sentient_analyzer.polarity_scores(sentence)
+        scores.pop('compound', None)
 
-    maxAttribute = max(scores, key=lambda k: scores[k])
+        maxAttribute = max(scores, key=lambda k: scores[k])
 
-    if maxAttribute == "neu":
-        neutral += 1
-    elif maxAttribute == "neg":
-        negative += 1
-    else:
-        positive += 1
+        if maxAttribute == "neu":
+            neutral += 1
+        elif maxAttribute == "neg":
+            negative += 1
+        else:
+            positive += 1
 
-total = neutral + negative + positive
-print("Negative: {0}% | Neutral: {1}% | Positive: {2}%".format(
-    negative*100/total, neutral*100/total, positive*100/total))
+    total = neutral + negative + positive
+    print("Negative: {0}% | Neutral: {1}% | Positive: {2}%".format(
+        negative*100/total, neutral*100/total, positive*100/total))
 
-labels = 'Neutral', 'Negative', 'Positive'
-sizes = [neutral, negative, positive]
-colors = ['#00bcd7', '#F57C00', '#CDDC39']
+    labels = 'Neutral', 'Negative', 'Positive'
+    sizes = [neutral, negative, positive]
+    colors = ['#00bcd7', '#F57C00', '#CDDC39']
 
-# Plot
-plt.pie(sizes, labels=labels, colors=colors,
-        autopct='%1.1f%%', startangle=140)
+    # Plot
+    plt.pie(sizes, labels=labels, colors=colors,
+            autopct='%1.1f%%', startangle=140)
 
-plt.axis('equal')
-plt.title("Sentiment Analysis - Chat with Innaya")
-plt.show()
+    plt.axis('equal')
+    plt.title("Sentiment Analysis - Chat with Innaya")
+    plt.show()
+
+analyze('innaya')
