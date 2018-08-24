6e803c85ed5ef0a57ae295961e668a202c37efee
Abhishek Soni <abhisheksoni2720@gmail.com>
2018-08-24 20:50:22 +0530

    method-ify

--- timing.py
+++ timing.py
@@ -3,7 +3,6 @@ import sys
 
 import matplotlib.pyplot as plt
 
-
 def split_text(filename):
     """
     Split file contents by newline.
@@ -12,24 +11,7 @@ def split_text(filename):
     chatText = chat.read()
     return chatText.splitlines()
 
-
-def analyze(name):
-    AM = []
-    PM = []
-    linesText = split_text(name + '.txt')
-    timeRegex = re.compile("(\d+(:)\d+)(\s)(\w+)")
-
-    # Distribute into AM and PM
-    for index, line in enumerate(linesText):
-        matches = re.findall(timeRegex, line)
-        if(len(matches) > 0):
-            match = matches[0]  # match now contains ('6:10', ':', ' ', 'PM')
-
-            if "AM" in match:
-                AM.append(match[0])
-            else:
-                PM.append(match[0])
-
+def groupByHour(AM, PM):
     # Now group time into 1-hour intervals
     time_groups = {}
 
@@ -64,10 +46,23 @@ def analyze(name):
         current_hour = str(current_hour)
         time_groups[current_hour] += 1
 
-    print(time_groups)
+    return time_groups
 
-    # Plot
+def distributeByAmPm(linesText, timeRegex):
+    AM, PM = [], []
+    for index, line in enumerate(linesText):
+        matches = re.findall(timeRegex, line)
+        if(len(matches) > 0):
+            match = matches[0]  # match now contains ('6:10', ':', ' ', 'PM')
 
+            if "AM" in match:
+                AM.append(match[0])
+            else:
+                PM.append(match[0])
+    
+    return AM, PM
+
+def plot_graph(time_groups, name):
     plt.bar(range(len(time_groups)), list(
         time_groups.values()), align='center')
 
@@ -77,5 +72,18 @@ def analyze(name):
     plt.ylabel('Frequency')
     plt.title("Timing Analysis - Chat with {0}".format(name.capitalize()))
     plt.show()
+
+def analyze(name):
+    linesText = split_text(name + '.txt')
+    timeRegex = re.compile("(\d+(:)\d+)(\s)(\w+)")
+
+    # Distribute into AM and PM
+    AM, PM = distributeByAmPm(linesText, timeRegex)
+
+    # Now group time into 1-hour intervals
+    time_groups = groupByHour(AM, PM)
+
+    # Plot
+    plot_graph(time_groups, name)
 
 analyze(sys.argv[1])
